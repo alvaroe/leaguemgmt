4dc7a73f321764aaa550d9d5712e087825fae6ff
Alvaro E. Escobar <alvaro@sportsystemsonline.com>
2012-02-20 18:54:43 -0500

    rails 3.2.1 barebones

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leaguemgmt::Application.config.secret_token = '79071c3470035cfd382d9022bf67d3a53e52df1c0e5711f4bf89b231784ca0bd68e7470553b890a1a73c767ab4060c6d421b75b9bed0870eb9dd47f4151e44fa'
